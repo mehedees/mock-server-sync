9e5167d316f999f6fe411d5c7068368aa13a9385
mehedee <mehedees@live.com>
2022-11-20 00:56:29 +0600

    Updated api with request serial

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from datetime import datetime
 app = FastAPI()
 
 
-@app.get("/")
-def mock_get(request: Request):
-    _msg = f"GET request received from {request.client} at {datetime.now().isoformat()}"
+@app.get("/{serial}")
+def mock_get(request: Request, serial: int):
+    _msg = f"GET request with #{serial}# received from {request.client} at {datetime.now().isoformat()}"
     logger.info(_msg)
     return {
         "success": True,
